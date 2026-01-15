d9993eb1d3dade05452165d5798ae0c20b5a01d2
Mohid-Abbas <mohidabbas1001@gmail.com>
2026-01-16 03:30:03 +0500

    Auto-update from Drive: 2026-01-15T22:30:03.386Z

--- js/data.js
+++ js/data.js
@@ -1,23 +1,10 @@
 /**
  * AUTO-GENERATED FILE - DO NOT EDIT MANUALLY
- * Last updated: 2026-01-15T22:23:55.907Z
+ * Last updated: 2026-01-15T22:30:03.235Z
  * Auto-synced from Google Drive
  */
 
 let projects = [
-  {
-    "id": "cloth-cabinet",
-    "title": "Cloth Cabinet",
-    "category": "cloth cabinet",
-    "description": "Exclusive collection of Cloth Cabinet",
-    "details": {
-      "Type": "Premium Design Collection"
-    },
-    "images": [
-      "https://lh3.googleusercontent.com/d/1nT__nq67mRvcy0Gpusov3nVE7Y8IZKA4",
-      "https://lh3.googleusercontent.com/d/1H_UM4gNSsu4PLTEA1i2MJTlp69ZneHSj"
-    ]
-  },
   {
     "id": "bedroom-wall",
     "title": "Bedroom Wall",
@@ -27,7 +14,21 @@ let projects = [
       "Type": "Premium Design Collection"
     },
     "images": [
-      "https://lh3.googleusercontent.com/d/1fxOrqnpj2C-cZxXwD8On9NdV0JkW_94n"
+      "https://lh3.googleusercontent.com/d/10Rn1pxlA43ATzowJSucvxirmcdBwpmw_",
+      "https://lh3.googleusercontent.com/d/16lzbcgw7RyrVUt8QxIc08zUYMTwUCjtJ",
+      "https://lh3.googleusercontent.com/d/18PCEUD8ygLgFPS88QzwXp8RVjd9Jlsu6",
+      "https://lh3.googleusercontent.com/d/1BVIq5MvIYFUw92c5k21sR2bn4ol-qGWN",
+      "https://lh3.googleusercontent.com/d/1H5eEAy-xCxo5dHJhkXYrUl5RjYRZIoRJ",
+      "https://lh3.googleusercontent.com/d/1QZUDi85nkQKgdfEqFfChCQK_byZpg4B_",
+      "https://lh3.googleusercontent.com/d/1RQ-PkxbYHLbN1E56YLwxOna9ezEQeC0J",
+      "https://lh3.googleusercontent.com/d/1Vk53X3HPih4iX5-Dy5zJiAepc9XAlftt",
+      "https://lh3.googleusercontent.com/d/1X34DV4Y0jRmWxHgroTVmUo3D5aahnOcY",
+      "https://lh3.googleusercontent.com/d/1elHWg1bjG5C03rXbTq6u1_92mQNeIarv",
+      "https://lh3.googleusercontent.com/d/1gZy9KFWQAv164mHI-urI-Sqdl10Gja5J",
+      "https://lh3.googleusercontent.com/d/1i7wU1jw3D8axMOdjBhCBGgfUvdwpVZCX",
+      "https://lh3.googleusercontent.com/d/1iVE64G0c9HpsPavxpZNVCiC7E-LLsZv2",
+      "https://lh3.googleusercontent.com/d/1lqdxGI6_zgKUGG3Qfx-qCdDxkrc3Rtt0",
+      "https://lh3.googleusercontent.com/d/1mOtg-_Hz73IJucQDDVUW_xEhT4FLzZwZ"
     ]
   }
 ];
