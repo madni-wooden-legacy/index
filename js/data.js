9bc98a52e0c6db8282445367d7bb61d1a5e7bbca
Mohid-Abbas <mohidabbas1001@gmail.com>
2026-01-16 03:40:49 +0500

    Auto-update from Drive: 2026-01-15T22:40:49.367Z

--- js/data.js
+++ js/data.js
@@ -1,10 +1,24 @@
 /**
  * AUTO-GENERATED FILE - DO NOT EDIT MANUALLY
- * Last updated: 2026-01-15T22:30:03.235Z
+ * Last updated: 2026-01-15T22:40:49.224Z
  * Auto-synced from Google Drive
  */
 
 let projects = [
+  {
+    "id": "mirror",
+    "title": "Mirror",
+    "category": "mirror",
+    "description": "Exclusive collection of Mirror",
+    "details": {
+      "Type": "Premium Design Collection"
+    },
+    "images": [
+      "https://lh3.googleusercontent.com/d/1o0vjtUewjjaZDwtv4mq9_FmGaTglxbUZ",
+      "https://lh3.googleusercontent.com/d/1wj_cvyohe7wFzUlWmb-qcK3cByhSyCOS",
+      "https://lh3.googleusercontent.com/d/1y21lTCOW1TAzeBEnxJP7412TQ2tEEDcJ"
+    ]
+  },
   {
     "id": "bedroom-wall",
     "title": "Bedroom Wall",
@@ -29,6 +43,29 @@ let projects = [
       "https://lh3.googleusercontent.com/d/1iVE64G0c9HpsPavxpZNVCiC7E-LLsZv2",
       "https://lh3.googleusercontent.com/d/1lqdxGI6_zgKUGG3Qfx-qCdDxkrc3Rtt0",
       "https://lh3.googleusercontent.com/d/1mOtg-_Hz73IJucQDDVUW_xEhT4FLzZwZ"
+    ]
+  },
+  {
+    "id": "doors",
+    "title": "Doors",
+    "category": "doors",
+    "description": "Exclusive collection of Doors",
+    "details": {
+      "Type": "Premium Design Collection"
+    },
+    "images": [
+      "https://lh3.googleusercontent.com/d/10vSLMu2csNwYAv579p9XXcfQeMekoet1",
+      "https://lh3.googleusercontent.com/d/1OC9f5iGl09RUBJD7dXX_MgOPtPqygsQz",
+      "https://lh3.googleusercontent.com/d/1P6GQOhojGLDx2izZ5lqeN3JXUyyT_A_n",
+      "https://lh3.googleusercontent.com/d/1b-mQ5iN_S7aeYAjxqX-OqezVDPDjtsBc",
+      "https://lh3.googleusercontent.com/d/1hii5JejedwGbae09XRpnqMaxNoz9p5yO",
+      "https://lh3.googleusercontent.com/d/1iyJjooXuo5i2CVmvjGQ5hXcAuYT8t-nw",
+      "https://lh3.googleusercontent.com/d/1jVMfoEmb-O2HTjculU6nG_TFmFjOz3u-",
+      "https://lh3.googleusercontent.com/d/1lRBmVSgyu6sZhm6z2wdNy8CblWG5l1WI",
+      "https://lh3.googleusercontent.com/d/1pvRagjRlJbTJWdJhJuaxzLTQJ_i4AdXM",
+      "https://lh3.googleusercontent.com/d/1rfl8iQ1hiV3LzwMTciKt_N1rhRVEDQ4q",
+      "https://lh3.googleusercontent.com/d/1rpphzZGiZYNG1_Tmk8rGqAsLdOhLU7AO",
+      "https://lh3.googleusercontent.com/d/1sH9YgidK5M_0gbWeZy9e8nbSNFReO11y"
     ]
   }
 ];
